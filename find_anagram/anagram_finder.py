9ec9515ca265509dc86c860de8d89907c2ed4a14
fyiyu091 <yiyu091@gmail.com>
2019-06-25 22:13:08 -0700

    add find_anagram project

---
+++ find_anagram/anagram_finder.py
@@ -0,0 +1,17 @@
+import load_file
+
+word_list = load_file.load("dictionary.txt")
+
+anagram_list = []
+
+input_name = "forest"
+
+for word in word_list:
+    if word != input_name:
+        if sorted(word) == sorted(input_name):
+            anagram_list.append(word)
+
+if len(anagram_list) == 0:
+    print("there is no anagram of the input name")
+else:
+    print("anagram = ", *anagram_list, sep='\n')
